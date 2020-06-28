5026675ccdc38b97955a5410bd64f4f768ab6f1d
hamnakazmi297 <hamnakazmi297@gmail.com>
2020-06-28 14:56:16 +0500

    Calculator App using javascript

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Name: Hamna Kazmi
+// Roll No: WM3857
+
 function getNum(num) {
     var res = document.getElementById("res");
     if (res.value == null || res.value == 0)
@@ -5,21 +8,42 @@ function getNum(num) {
     else
         res.value += num;
 }
+
 function sqRoot() {
-	res.value = Math.sqrt(res.value);
+    res.value = Math.sqrt(res.value);
 }
 
 function expo() {
-	res.value = Math.exp(res.value);
+    res.value = Math.exp(res.value);
 }
 
-function evalResult(){
+function evalResult() {
     var res = document.getElementById("res");
     res.value = eval(res.value);
 }
 
-
-function del(res){
+function sq() {
     var res = document.getElementById("res");
-    res.value = res.slice(0,-1);
+    res.value = res.value * res.value;
+}
+
+function signChange() {
+    var res = document.getElementById("res");
+    if (res.value.slice(0, 1) == "-") {
+        res.value = res.value.replace("-", "");
+    }
+    else {
+        res.value = "-" + res.value;
+    }
+}
+
+function DivByOne() {
+    var res = document.getElementById("res");
+        res.value = 1/res.value;
+}
+
+function del() {
+    var res = document.getElementById("res");
+    var a = res.value;
+    res.value = a.slice(0, -1);
 }
